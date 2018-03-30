710ff83acbba454c4c2faffad388e8fdbccc2c1a
jirolrol <jirolrol@gmail.com>
2018-03-30 15:00:05 +0900

    change stack.js

---
+++ stack.js
@@ -0,0 +1,18 @@
+function Stack() {
+    var items = [];
+
+    this.enqueue = function(element) {
+        items.push(element);
+    }
+    this.dequeue = function() {
+        return items.shift();
+    }
+    this.front = function() {
+        return items[0];
+    }
+    this.isEmpty = function() {
+        return items.length == 0;
+    }
+}
+
+var stack = new Stack();
